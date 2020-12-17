54d4f0add2ed7458c66908b02ff0515851a72fdf
sunnycloudsocks <74031254+sunnycloudsocks@users.noreply.github.com>
2020-12-17 13:29:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var play = document.getElementByClassName("play");
 play.addEventListener("mouseenter", enlarge);
 function enlarge(){
   document.getElementByClassName("play").className = "enlargePlay";
-  document.getElementByClassName("play").backgroundColor = "red";
+  document.getElementByClassName("play").style.backgroundColor = "red";
 }
