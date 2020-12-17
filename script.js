7c2d95b27ce066d521cdc96480eece35488ea396
sunnycloudsocks <74031254+sunnycloudsocks@users.noreply.github.com>
2020-12-17 10:27:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-var play = 
+var play = document.getElementByClassName("play");
+play.addEventListener("mouseenter", enlarge);
+function enlarge(){
+  document.getElementByClassName("play").className = "enlargePlay"
+}
